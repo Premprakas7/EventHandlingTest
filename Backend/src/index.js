49004f8bb0c46f3280de4e575ed34f554ccfc1f1
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-18 16:00:08 +0530

    user controller added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,11 @@
 const express=require("express");
+const userController=require("./controllers/user.controller")
+const cors=require("cors")
+
 const app=express()
+app.use(express.json());
+app.use(cors())
 
-
-
-
+app.use("/users", userController)
 
 module.exports=app;
