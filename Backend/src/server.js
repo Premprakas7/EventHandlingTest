a7e2d0a87b873f72187b7b277f449209dcb015a7
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-18 13:40:23 +0530

    dependencies installed

---
+++ Backend/src/server.js
@@ -0,0 +1,10 @@
+const app = require("./index");
+
+app.listen(5000, async function () {
+  try {
+    await connect();
+    console.log("listening on port 5000");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
