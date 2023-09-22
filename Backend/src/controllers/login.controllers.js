540fddc5f13fb6758d60d60b3ef277152ae1c2bb
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-22 12:47:49 +0530

    updated

--- Backend/src/controllers/login.controllers.js
+++ Backend/src/controllers/login.controllers.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 const User = require('../models/user.model')
 const secreteKey = 'secreteKey'
 
