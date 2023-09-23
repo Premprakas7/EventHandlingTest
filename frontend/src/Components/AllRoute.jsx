72e536a4af4108b13d6c2b1b4a699f13d24451e3
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-23 14:30:16 +0530

    updated

--- frontend/src/Components/AllRoute.jsx
+++ frontend/src/Components/AllRoute.jsx
@@ -3,12 +3,13 @@ import {Route, Routes} from "react-router-dom"
 import Dashboard from './Dashboard'
 import Login from './Login'
 import Signup from './Signup'
+import ReqAuth from './ReqAuth'
 
 const AllRoute = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/' element={<ReqAuth><Dashboard/></ReqAuth>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/signup' element={<Signup/>}/>
             
