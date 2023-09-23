033b34eb3734196bb669648927aee3fd5b7ec5b5
Premprakas7 <97504560+Premprakas7@users.noreply.github.com>
2023-09-23 09:47:58 +0530

    configs added

--- Backend/src/configs/db.js
+++ Backend/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://prakash:prem@cluster0.ihf7myh.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://prem:prem@cluster0.fkthv.mongodb.net/eventhandling"
   )
 };
 
